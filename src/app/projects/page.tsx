e1bb1712e34d8f6b76f52aa9651d2b8329dd5b3f
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-11 21:47:52 -0300

    add aa to projects, remove agr

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,19 +14,27 @@ const Projects: FunctionComponent = () => {
         <div className="grid grid-cols-2 gap-10 md:grid-cols-1">
           <SingleProject
             title="UI Kit"
-            description="React component library and custom hooks. Personally, I'm not a big fan of any component library, so I've been creating my own components. Being the owner of my code makes it easier for me to customize and extend them based on my needs. It offers typical components of a design system out of the box such as Dialog, Alert, Accordion, Button, etc. I might consider publishing it on npm."
+            description="React component library and custom hooks. Personally, I'm not a big fan of any component library, so I've been creating my own components. Being the owner of my code makes it easier for me to customize and extend them based on my needs. It offers typical components of a design system out of the box such as Dialog, Alert, Accordion, Button, etc."
             image="/components.webp"
             projectLink="https://ui-kit7.vercel.app"
             codeLink="https://github.com/aguirrealvaro/ui-kit"
             imgClassname="object-cover"
           />
           <SingleProject
+            title="aguirrealvaro.dev"
+            description="My personal website and blog. With a visit counter and a like button for each post."
+            image="/aa2.jpg"
+            projectLink="https://www.aguirrealvaro.dev/"
+            codeLink="https://github.com/aguirrealvaro/aguirrealvaro"
+            imgClassname="object-contain bg-black"
+          />
+          {/* <SingleProject
             title="Estudio AGR"
             description="Web page of a real estate company"
             image="/agr.jpeg"
             projectLink="https://www.estudioagr.com/"
             imgClassname="object-contain bg-white"
-          />
+          /> */}
         </div>
       </Wrapper>
     </PageContainer>
