6ca345cf91b4121a46a980ce8ca28c695e2fd75d
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-16 20:40:38 -0300

    enabled hover on icon button

--- src/components/ui/icon-button/icon-button.tsx
+++ src/components/ui/icon-button/icon-button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/utils/cn";
 
 const getIconButtonVariants = cva(
-  ["flex rounded", "transition hover:bg-hover-primary", "disabled:cursor-not-allowed"],
+  ["flex rounded", "transition enabled:hover:bg-hover-primary", "disabled:cursor-not-allowed"],
   {
     variants: {
       size: {
