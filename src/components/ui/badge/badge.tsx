30e04d2836c04e1d8b99a203cb5857c122f2ddcf
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-16 23:18:21 -0300

    change badge styles

--- src/components/ui/badge/badge.tsx
+++ src/components/ui/badge/badge.tsx
@@ -9,9 +9,9 @@ const Badge = ({ children }: BadgeProps) => {
   return (
     <span
       className={cn(
-        "bg-neutral-700 dark:bg-neutral-400",
-        "text-neutral-300 dark:text-neutral-700",
-        "text-nowrap rounded-md px-2 py-0.5 font-mono text-xs font-semibold"
+        "text-nowrap rounded-md px-2 py-0.5 font-mono text-xs font-semibold",
+        "bg-neutral-200 dark:bg-neutral-700",
+        "text-neutral-700 dark:text-neutral-200"
       )}
     >
       {children}
