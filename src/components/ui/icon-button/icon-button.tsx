3f93ba8990aaa15c6eb35c7f9ea86c34a0859f4d
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-18 20:33:05 -0300

    remove enabled class on icon button

--- src/components/ui/icon-button/icon-button.tsx
+++ src/components/ui/icon-button/icon-button.tsx
@@ -5,7 +5,12 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/cn";
 
 const getIconButtonVariants = cva(
-  ["flex rounded", "transition enabled:hover:bg-hover-primary", "disabled:cursor-not-allowed"],
+  [
+    "flex rounded",
+    "transition hover:bg-hover-primary",
+    "disabled:cursor-not-allowed",
+    "disabled:hover:bg-transparent",
+  ],
   {
     variants: {
       size: {
