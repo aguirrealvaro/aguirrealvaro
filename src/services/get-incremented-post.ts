ac03fb3a64cb4ae502859a2044df1ac5fa4caf8a
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-01-19 02:06:04 -0300

    tw eslint running on cv and cva

--- src/services/get-incremented-post.ts
+++ src/services/get-incremented-post.ts
@@ -3,6 +3,7 @@ import type { PostWithLikes } from "@/types";
 
 // get post with the incremented view
 export const getIncrementedPost = async (slug: string): Promise<PostWithLikes> => {
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
   const post = await prisma.post.update({
     where: { slug },
     data: { views: { increment: 1 } },
