d075ad3ba9a4ab692c397a2e6538df12134d1f63
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2024-03-10 21:52:20 -0300

    change database to supabase

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,9 +4,8 @@ const Footer = () => {
   return (
     <footer>
       <Wrapper>
-        <div className="flex flex-col items-center py-4 text-center text-sm text-text-secondary">
+        <div className="py-4 text-center text-sm text-text-secondary">
           <span>Made with Next, React Server Components, Server Actions & MDX</span>
-          <span>Deployed to Vercel & PlanetScale</span>
         </div>
       </Wrapper>
     </footer>
